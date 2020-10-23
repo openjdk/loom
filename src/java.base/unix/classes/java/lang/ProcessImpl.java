561ace1bb64e14d48e26de12b7b71779954e6f7a
Alan Bateman <alan.bateman@oracle.com>
2020-10-23 11:01:32 +0100

    Process::waitFor

--- src/java.base/unix/classes/java/lang/ProcessImpl.java
+++ src/java.base/unix/classes/java/lang/ProcessImpl.java
@@ -47,6 +47,9 @@ import java.security.PrivilegedAction;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.util.Properties;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 import jdk.internal.access.JavaIOFileDescriptorAccess;
 import jdk.internal.access.SharedSecrets;
 import jdk.internal.util.StaticProperty;
@@ -72,6 +75,9 @@ final class ProcessImpl extends Process {
     private final ProcessHandleImpl processHandle;
     private int exitcode;
     private boolean hasExited;
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
 
     private /* final */ OutputStream stdin;
     private /* final */ InputStream  stdout;
@@ -360,10 +366,13 @@ final class ProcessImpl extends Process {
                          new ProcessPipeInputStream(fds[2]);
 
                 ProcessHandleImpl.completion(pid, true).handle((exitcode, throwable) -> {
-                    synchronized (this) {
+                    lock.lock();
+                    try {
                         this.exitcode = (exitcode == null) ? -1 : exitcode.intValue();
                         this.hasExited = true;
-                        this.notifyAll();
+                        condition.signalAll();
+                    } finally {
+                        lock.unlock();
                     }
 
                     if (stdout instanceof ProcessPipeInputStream)
@@ -393,10 +402,13 @@ final class ProcessImpl extends Process {
                          new DeferredCloseProcessPipeInputStream(fds[2]);
 
                 ProcessHandleImpl.completion(pid, true).handle((exitcode, throwable) -> {
-                    synchronized (this) {
+                    lock.lock();
+                    try {
                         this.exitcode = (exitcode == null) ? -1 : exitcode.intValue();
                         this.hasExited = true;
-                        this.notifyAll();
+                        condition.signalAll();
+                    } finally {
+                        lock.unlock();
                     }
 
                     if (stdout instanceof DeferredCloseProcessPipeInputStream)
@@ -428,37 +440,43 @@ final class ProcessImpl extends Process {
         return stderr;
     }
 
-    public synchronized int waitFor() throws InterruptedException {
-        while (!hasExited) {
-            wait();
+    public int waitFor() throws InterruptedException {
+        lock.lock();
+        try {
+            while (!hasExited) {
+                condition.await();
+            }
+            return exitcode;
+        } finally {
+            lock.unlock();
         }
-        return exitcode;
     }
 
-    @Override
-    public synchronized boolean waitFor(long timeout, TimeUnit unit)
+    public boolean waitFor(long timeout, TimeUnit unit)
         throws InterruptedException
     {
-        long remainingNanos = unit.toNanos(timeout);    // throw NPE before other conditions
-        if (hasExited) return true;
-        if (timeout <= 0) return false;
-
-        long deadline = System.nanoTime() + remainingNanos;
-        do {
-            TimeUnit.NANOSECONDS.timedWait(this, remainingNanos);
-            if (hasExited) {
-                return true;
+        lock.lock();
+        try {
+            long remainingNanos = unit.toNanos(timeout);    // throw NPE before other conditions
+            while (remainingNanos > 0 && !hasExited) {
+                remainingNanos = condition.awaitNanos(remainingNanos);
             }
-            remainingNanos = deadline - System.nanoTime();
-        } while (remainingNanos > 0);
-        return hasExited;
+            return hasExited;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized int exitValue() {
-        if (!hasExited) {
-            throw new IllegalThreadStateException("process hasn't exited");
+    public int exitValue() {
+        lock.lock();
+        try {
+            if (!hasExited) {
+                throw new IllegalThreadStateException("process hasn't exited");
+            }
+            return exitcode;
+        } finally {
+            lock.unlock();
         }
-        return exitcode;
     }
 
     private void destroy(boolean force) {
@@ -472,9 +490,12 @@ final class ProcessImpl extends Process {
                 // there is an unavoidable race condition here, but the window
                 // is very small, and OSes try hard to not recycle pids too
                 // soon, so this is quite safe.
-                synchronized (this) {
+                lock.lock();
+                try {
                     if (!hasExited)
                         processHandle.destroyProcess(force);
+                } finally {
+                    lock.unlock();
                 }
                 try { stdin.close();  } catch (IOException ignored) {}
                 try { stdout.close(); } catch (IOException ignored) {}
@@ -537,8 +558,13 @@ final class ProcessImpl extends Process {
     }
 
     @Override
-    public synchronized boolean isAlive() {
-        return !hasExited;
+    public boolean isAlive() {
+        lock.lock();
+        try {
+            return !hasExited;
+        } finally {
+            lock.unlock();
+        }
     }
 
     /**
