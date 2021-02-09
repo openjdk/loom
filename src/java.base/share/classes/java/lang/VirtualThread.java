64cae64641d0ccad25eba1ccd31306c385c3c8f3
Alan Bateman <alan.bateman@oracle.com>
2021-02-09 08:10:57 +0000

    Mount/unmount changed to run with continuation mounted

--- src/java.base/share/classes/java/lang/VirtualThread.java
+++ src/java.base/share/classes/java/lang/VirtualThread.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -122,15 +122,7 @@ class VirtualThread extends Thread {
      */
     VirtualThread(Executor scheduler, String name, int characteristics, Runnable task) {
         super(name, characteristics);
-
         Objects.requireNonNull(task);
-        Runnable target = () -> {
-            try {
-                task.run();
-            } catch (Throwable exc) {
-                dispatchUncaughtException(exc);
-            }
-        };
 
         // choose scheduler if not specified
         if (scheduler == null) {
@@ -143,7 +135,7 @@ class VirtualThread extends Thread {
         }
 
         this.scheduler = scheduler;
-        this.cont = new VThreadContinuation(this, target);
+        this.cont = new VThreadContinuation(this, task);
         if (scheduler != DEFAULT_SCHEDULER) {
             this.runContinuation = new CustomRunner(this);
         } else {
@@ -166,27 +158,42 @@ class VirtualThread extends Thread {
     }
 
     /**
-     * A continuation for a virtual thread.
+     * The continuation that a virtual thread executes.
      */
     private static class VThreadContinuation extends Continuation {
-        private final VirtualThread vthread;
         VThreadContinuation(VirtualThread vthread, Runnable task) {
-            super(VTHREAD_SCOPE, task);
-            this.vthread = vthread;
+            super(VTHREAD_SCOPE, new TaskWrapper(vthread, task));
         }
-
         @Override
         protected void onPinned(Continuation.Pinned reason) {
             if (TRACE_PINNING_MODE > 0) {
                 boolean printAll = (TRACE_PINNING_MODE == 1);
                 PinnedThreadPrinter.printStackTrace(System.out, printAll);
             }
+        }
+    }
 
-            int s = vthread.state();
-            if (s == PARKING) {
-                vthread.parkOnCarrierThread();
-            } else if (s == YIELDING) {
-                vthread.setState(RUNNING);
+    /**
+     * Wraps a task so that it executes in the context of a virtual thread.
+     * The virtual thread is mounted before the task runs, then unmounts when
+     * the task completes.
+     */
+    private static class TaskWrapper implements Runnable {
+        private final VirtualThread vthread;
+        private final Runnable task;
+        TaskWrapper(VirtualThread vthread, Runnable task) {
+            this.vthread = vthread;
+            this.task = task;
+        }
+        @ChangesCurrentThread
+        public void run() {
+            vthread.mount(true);
+            try {
+                task.run();
+            } catch (Throwable exc) {
+                vthread.dispatchUncaughtException(exc);
+            } finally {
+                vthread.unmount();
             }
         }
     }
@@ -230,7 +237,6 @@ class VirtualThread extends Thread {
     /**
      * Runs or continues execution of the continuation on the current thread.
      */
-    @ChangesCurrentThread
     private void runContinuation() {
         // the carrier thread should be a platform thread
         if (Thread.currentThread().isVirtual()) {
@@ -248,12 +254,9 @@ class VirtualThread extends Thread {
             throw new IllegalStateException();
         }
 
-        boolean firstMount = (initialState == STARTED);
-        mount(firstMount);
         try {
             cont.run();
         } finally {
-            unmount();
             if (cont.isDone()) {
                 afterTerminate(/*executed*/ true);
             } else {
@@ -328,14 +331,27 @@ class VirtualThread extends Thread {
         }
     }
 
+    /**
+     * Unmounts this virtual thread, invokes Continuation.yield, and re-mounts the
+     * thread when continued.
+     */
+    @ChangesCurrentThread
     private void yieldContinuation() {
+        unmount();
+        boolean yielded = false;
         try {
-            Continuation.yield(VTHREAD_SCOPE);
+            yielded = Continuation.yield(VTHREAD_SCOPE);
         } finally {
-            // restore state in case of OutOfMemoryError/other errors
-            if (state() != RUNNING) {
-                setState(RUNNING);
+            mount(false);
+            if (!yielded) {
+                // pinned or resource error
+                if (state() == PARKING) {
+                    parkOnCarrierThread();
+                } else {
+                    setState(RUNNING);
+                }
             }
+            assert (Thread.currentThread() == this) && (state() == RUNNING);
         }
     }
 
