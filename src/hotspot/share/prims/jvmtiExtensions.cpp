2324c590fcba2258cf0158f1a37d73e44020160a
Serguei Spitsyn <serguei.spitsyn@oracle.com>
2021-03-18 02:48:56 +0000

    fix build errors caused by reimpl of JVMTI functions with extension mechanism

--- src/hotspot/share/prims/jvmtiExtensions.cpp
+++ src/hotspot/share/prims/jvmtiExtensions.cpp
@@ -23,8 +23,10 @@
  */
 
 #include "precompiled.hpp"
+#include "classfile/javaClasses.inline.hpp"
 #include "prims/jvmtiExport.hpp"
 #include "prims/jvmtiExtensions.hpp"
+#include "prims/jvmtiThreadState.inline.hpp"
 #include "runtime/interfaceSupport.inline.hpp"
 #include "runtime/handles.inline.hpp"
 #include "runtime/jniHandles.inline.hpp"
