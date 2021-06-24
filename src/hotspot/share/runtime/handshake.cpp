1e12c1066a416ecb1f89b716bc6164b2e7afc597
Coleen Phillimore <coleen.phillimore@oracle.com>
2021-06-24 14:12:50 -0400

    Add comment about threadObj.

--- src/hotspot/share/runtime/handshake.cpp
+++ src/hotspot/share/runtime/handshake.cpp
@@ -628,6 +628,8 @@ class ThreadSelfSuspensionHandshake : public AsyncHandshakeClosure {
 };
 
 bool HandshakeState::suspend_with_handshake(JavaThread* caller) {
+  // This tested for _handshakee->threadObj() != NULL as well, but the test doesn't work
+  // for that.  TODO: can you suspend a thread during initialization ?
   if (_handshakee->is_exiting()) {
     log_trace(thread, suspend)("JavaThread:" INTPTR_FORMAT " exiting", p2i(_handshakee));
     return false;
