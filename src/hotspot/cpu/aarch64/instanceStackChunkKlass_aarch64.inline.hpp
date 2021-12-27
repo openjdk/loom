5d0a7b539bbd729a9da6f008f9491c5e3a0ef20b
Alan Bateman <alan.bateman@oracle.com>
2021-12-27 07:36:34 +0000

    More build configurations

--- src/hotspot/cpu/aarch64/instanceStackChunkKlass_aarch64.inline.hpp
+++ src/hotspot/cpu/aarch64/instanceStackChunkKlass_aarch64.inline.hpp
@@ -170,7 +170,7 @@ class SmallRegisterMap {
 public:
   static constexpr SmallRegisterMap* instance = nullptr;
 private:
-  static void assert_is_rfp(VMReg r) PRODUCT_RETURN 
+  static void assert_is_rfp(VMReg r) NOT_DEBUG_RETURN
                                      DEBUG_ONLY({ assert (r == rfp->as_VMReg() || r == rfp->as_VMReg()->next(), "Reg: %s", r->name()); })
 public:
   // as_RegisterMap is used when we didn't want to templatize and abstract over RegisterMap type to support SmallRegisterMap
