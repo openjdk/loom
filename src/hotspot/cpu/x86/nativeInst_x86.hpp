10d3be1d6271c559fa89940a3435c857186cca06
Rickard Bäckman <rickard.backman@oracle.com>
2022-01-28 02:14:21 +0100

    Fix overwriting problems at deopt

--- src/hotspot/cpu/x86/nativeInst_x86.hpp
+++ src/hotspot/cpu/x86/nativeInst_x86.hpp
@@ -759,7 +759,7 @@ class NativeDeoptInstruction: public NativeInstruction {
   enum Intel_specific_constants {
     instruction_prefix          = 0x0F,
     instruction_code            = 0xFF,
-    instruction_size            =    2,
+    instruction_size            =    3,
     instruction_offset          =    0,
   };
 
@@ -774,7 +774,7 @@ class NativeDeoptInstruction: public NativeInstruction {
   }
 
   // MT-safe patching
-  static void insert(address code_pos);
+  static void insert(address code_pos, bool invalidate = true);
 };
 
 #endif // CPU_X86_NATIVEINST_X86_HPP
