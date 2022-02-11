5f364c2d625f10120367368f30671574cda49b06
Alan Bateman <alan.bateman@oracle.com>
2022-02-11 09:26:54 +0000

    Misc. cleanup

--- src/java.base/share/native/include/jni.h
+++ src/java.base/share/native/include/jni.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -771,7 +771,7 @@ struct JNINativeInterface_ {
     jobject (JNICALL *GetModule)
        (JNIEnv* env, jclass clazz);
 
-    /* Loom */
+    /* Virtual threads */
 
     jboolean (JNICALL *IsVirtualThread)
        (JNIEnv* env, jobject obj);
