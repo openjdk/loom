43fda1fe54ff6c1f4bfd0469dd229ee90134d167
Alan Bateman <alan.bateman@oracle.com>
2022-02-11 09:44:13 +0000

    Missing @modules on JFR test

--- test/jdk/java/lang/Thread/virtual/JfrEvents.java
+++ test/jdk/java/lang/Thread/virtual/JfrEvents.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,7 +24,7 @@
 /**
  * @test
  * @summary Basic test for JFR jdk.VirtualThreadXXX events.
- * @modules java.base/java.lang:+open
+ * @modules jdk.jfr java.base/java.lang:+open
  * @compile --enable-preview -source ${jdk.version} JfrEvents.java TestHelper.java
  * @run testng/othervm --enable-preview JfrEvents
  */
