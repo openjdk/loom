e8a033ca3ad22f72e1d94b858f6e0cd4a320c769
Alan Bateman <alan.bateman@oracle.com>
2022-02-14 18:45:48 +0000

    Add assert to help debugging

--- src/java.base/windows/classes/sun/nio/ch/WSAPollPoller.java
+++ src/java.base/windows/classes/sun/nio/ch/WSAPollPoller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -175,7 +175,8 @@ class WSAPollPoller extends Poller {
         while ((fd = registerQueue.pollFirst()) != null) {
             short events = (reading()) ? Net.POLLIN : Net.POLLOUT;
             int index = add(fd, events);
-            fdToIndex.put(fd, index);
+            Integer previous = fdToIndex.put(fd, index);
+            assert previous == null;
         }
     }
 
