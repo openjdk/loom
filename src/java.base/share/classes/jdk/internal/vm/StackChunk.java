28aa2af997332d1e25dabf6561b708a57be0e605
Ron Pressler <ron.pressler@oracle.com>
2022-03-05 15:56:19 +0000

    -

--- src/java.base/share/classes/jdk/internal/vm/StackChunk.java
+++ src/java.base/share/classes/jdk/internal/vm/StackChunk.java
@@ -29,11 +29,11 @@ public final class StackChunk {
     public static void init() {}
 
     private StackChunk parent;
-    private int size; // in words
-    private int sp; // in words
+    private int size;    // in words
+    private int sp;      // in words
     private int argsize; // bottom stack-passed arguments, in words
 
-    // The stack itself is appended here by the VM
+    // The stack itself is appended here by the VM, as well as some injected fields
 
     public StackChunk parent() { return parent; }
     public boolean isEmpty()   { return sp >= (size - argsize); }
