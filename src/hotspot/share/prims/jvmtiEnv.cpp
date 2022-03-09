ac2866a24961d1217392b4cc1bf52ee4ee83d73d
Alan Bateman <alan.bateman@oracle.com>
2022-03-09 09:20:08 +0000

    Fix typo in comment

--- src/hotspot/share/prims/jvmtiEnv.cpp
+++ src/hotspot/share/prims/jvmtiEnv.cpp
@@ -1272,7 +1272,7 @@ JvmtiEnv::GetThreadInfo(jthread thread, jvmtiThreadInfo* info_ptr) {
   oop loader = java_lang_Thread::context_class_loader(thread_obj());
   if (loader != NULL) {
     // Do the same as Thread.getContextClassLoader and set context_class_loader to be
-    // the system class loader then the field value is the "not supported" placeholder.
+    // the system class loader when the field value is the "not supported" placeholder.
     if (loader == java_lang_Thread_Constants::get_NOT_SUPPORTED_CLASSLOADER()) {
       loader = SystemDictionary::java_system_loader();
     }
