openjdk/loom

commit bd25c94d618923d10c768ad7146202c2130b25e2

Move set_anchor to fix async stack walks and cleanup

src/hotspot/share/classfile/javaClasses.inline.hpp
@@ -333,6 +333,10 @@ inline jint jdk_internal_vm_StackChunk::maxSize(oop ref) {
   return ref->int_field(_maxSize_offset);
 }
 inline void jdk_internal_vm_StackChunk::set_maxSize(oop ref, jint value) {
+#ifdef ASSERT
+  jint old = maxSize(ref);
+  log_develop_trace(jvmcont)("%s max_size: %d -> %d", value >= old ? "add" : "sub", old, value);
+#endif
   ref->int_field_put(_maxSize_offset, value);
 }
 

src/hotspot/share/compiler/oopMap.cpp
@@ -740,17 +740,14 @@ bool OopMap::equals(const OopMap* other) const {
 }
 
 int ImmutableOopMapSet::find_slot_for_offset(int pc_offset) const {
+  // we might not have an oopmap at asynchronous (non-safepoint) stackwalks
   ImmutableOopMapPair* pairs = get_pairs();
-
   for (int i = 0; i < _count; ++i) {
     if (pairs[i].pc_offset() >= pc_offset) {
       ImmutableOopMapPair* last = &pairs[i];
-      return last->pc_offset() == pc_offset ? i
-                                            : -1; // this can happen at asynchronous (non-safepoint) stackwalks
+      return last->pc_offset() == pc_offset ? i : -1;
     }
   }
-
-  guarantee(false, "failed to find oopmap for pc");
   return -1;
 }
 

src/hotspot/share/oops/instanceStackChunkKlass.inline.hpp
@@ -249,18 +249,18 @@ inline void StackChunkFrameStream<mixed>::get_cb() {
     "index: %d sp: " INTPTR_FORMAT " sp offset: %d end offset: %d size: %d chunk sp: %d gc_flag: %d",
     _index, p2i(sp()), _chunk->to_offset(sp()), _chunk->to_offset(_chunk->bottom_address()), _chunk->stack_size(), _chunk->sp(), _chunk->is_gc_mode());
 
-#ifdef ASSERT
-  if (!(is_interpreted() || is_compiled() || is_stub())) {
-    tty->print_cr(">>> oops get_cb1 pc: %p", pc()); os::print_location(tty, (intptr_t)pc());
-    tty->print_cr(">>> Stream:"); print_on(tty);
-    tty->print_cr(">>> Chunk:"); _chunk->print_on(false, tty);
-  }
-  if (_cb != nullptr && _cb->frame_size() <= 0) { tty->print_cr(">>> oops get_cb2 cb: %p size: %d", _cb, _cb->frame_size()); _cb->print_value_on(tty); }
-  if (!(is_interpreted() || ((is_stub() || is_compiled()) && _cb->frame_size() > 0))) { tty->print_cr(">>> oops get_cb3 cb: %p", _cb); os::print_location(tty, (intptr_t)pc()); tty->print_cr("----"); _cb->print_value_on(tty); }
-  assert (is_interpreted() || is_compiled() || is_stub(), "");
-#endif
+// #ifdef ASSERT
+//   if (!(is_interpreted() || is_compiled() || is_stub())) {
+//     tty->print_cr(">>> oops get_cb1 pc: %p", pc()); os::print_location(tty, (intptr_t)pc());
+//     tty->print_cr(">>> Stream:"); print_on(tty);
+//     tty->print_cr(">>> Chunk:"); _chunk->print_on(false, tty);
+//   }
+//   if (_cb != nullptr && _cb->frame_size() <= 0) { tty->print_cr(">>> oops get_cb2 cb: %p size: %d", _cb, _cb->frame_size()); _cb->print_value_on(tty); }
+//   if (!(is_interpreted() || ((is_stub() || is_compiled()) && _cb->frame_size() > 0))) { tty->print_cr(">>> oops get_cb3 cb: %p", _cb); os::print_location(tty, (intptr_t)pc()); tty->print_cr("----"); _cb->print_value_on(tty); }
+//   assert (is_interpreted() || is_compiled() || is_stub(), "");
+// #endif
 
-  assert (is_interpreted() || ((is_stub() || is_compiled()) && _cb->frame_size() > 0),
+  assert (is_interpreted() || Continuation::is_return_barrier_entry(pc()) || ((is_stub() || is_compiled()) && _cb->frame_size() > 0),
     "index: %d sp: " INTPTR_FORMAT " sp offset: %d end offset: %d size: %d chunk sp: %d is_stub: %d is_compiled: %d frame_size: %d mixed: %d",
     _index, p2i(sp()), _chunk->to_offset(sp()), _chunk->to_offset(_chunk->bottom_address()), _chunk->stack_size(), _chunk->sp(), is_stub(), is_compiled(), _cb->frame_size(), mixed);
 }

src/hotspot/share/oops/stackChunkOop.cpp
@@ -49,7 +49,6 @@ bool stackChunkOopDesc::should_fix() const {
 }
 
 frame stackChunkOopDesc::top_frame(RegisterMap* map) {
-  // tty->print_cr(">>> stackChunkOopDesc::top_frame this: %p map: %p map->chunk: %p", this, map, (stackChunkOopDesc*)map->stack_chunk()());
   assert (!is_empty(), "");
   StackChunkFrameStream<true> fs(this);
 
@@ -58,6 +57,9 @@ frame stackChunkOopDesc::top_frame(RegisterMap* map) {
   // if (map->update_map() && should_fix()) InstanceStackChunkKlass::fix_frame<true, false>(fs, map);
 
   frame f = fs.to_frame();
+  // if (!maybe_fix_async_walk(f, map)) return frame();
+
+  assert (to_offset(f.sp()) == sp(), "f.offset_sp(): %d sp(): %d async: %d", f.offset_sp(), sp(), map->is_async());
   relativize_frame(f);
   f.set_frame_index(0);
   return f;
@@ -92,6 +94,25 @@ frame stackChunkOopDesc::sender(const frame& f, RegisterMap* map) {
 
   return Continuation::continuation_parent_frame(map);
 }
+
+// bool stackChunkOopDesc::maybe_fix_async_walk(frame& f, RegisterMap* map) const {
+//   if (!Continuation::is_return_barrier_entry(f.pc()))
+//     return true;
+
+//   // Can happen during async stack walks, where the continuation is in the midst of a freeze/thaw
+//   assert (map->is_async(), "");
+//   address pc0 = pc();
+
+//   // we write sp first, then pc; here we read in the opposite order, so if sp is right, so is pc.
+//   OrderAccess::loadload();
+//   if (sp() == to_offset(f.sp())) {
+//     f.set_pc(pc0);
+//     return true;
+//   }
+//   assert (false, "");
+//   log_debug(jvmcont)("failed to fix frame during async stackwalk");
+//   return false;
+// }
 
 static int num_java_frames(CompiledMethod* cm, address pc) {
   int count = 0;

src/hotspot/share/oops/stackChunkOop.hpp
@@ -154,6 +154,8 @@ private:
 
   inline void relativize_frame_pd(frame& fr) const;
   inline void derelativize_frame_pd(frame& fr) const;
+
+  bool maybe_fix_async_walk(frame& f, RegisterMap* map) const;
 };
 
 #ifndef CHECK_UNHANDLED_OOPS

src/hotspot/share/prims/jvmtiTagMap.cpp
@@ -1151,7 +1151,7 @@ void JvmtiTagMap::iterate_through_heap(jint heap_filter,
                                        const void* user_data)
 {
   // EA based optimizations on tagged objects are already reverted.
-  EscapeBarrier eb(false, JavaThread::current());
+  EscapeBarrier eb(false, JavaThread::current()); // disable due to virtual threads
   eb.deoptimize_objects_all_threads();
   MutexLocker ml(Heap_lock);
   IterateThroughHeapObjectClosure blk(this,

src/hotspot/share/runtime/continuation.hpp
@@ -67,7 +67,7 @@ public:
   static bool is_continuation_entry_frame(const frame& f, const RegisterMap *map);
 
   static bool is_mounted(JavaThread* thread, oop cont_scope);
-  
+
   static oop  get_continuation_for_frame(JavaThread* thread, const frame& f);
 
   static bool is_frame_in_continuation(ContinuationEntry* cont, const frame& f);
@@ -93,7 +93,7 @@ public:
   static bool fix_continuation_bottom_sender(JavaThread* thread, const frame& callee, address* sender_pc, intptr_t** sender_sp);
   static address get_top_return_pc_post_barrier(JavaThread* thread, address pc);
   static void set_cont_fastpath_thread_state(JavaThread* thread);
-  static void notify_deopt(JavaThread* thread, intptr_t* sp); 
+  static void notify_deopt(JavaThread* thread, intptr_t* sp);
 
   // access frame data
 
@@ -111,7 +111,6 @@ public:
   static bool debug_is_stack_chunk(oop obj);
   static bool debug_is_continuation(Klass* klass);
   static bool debug_is_continuation(oop obj);
-  static bool debug_is_continuation_run_frame(const frame& f);
   static bool debug_verify_continuation(oop cont);
   static void debug_print_continuation(oop cont, outputStream* st = NULL);
 #endif

commit 82917fb54d40a91b24fda07cfa3746e61ea38302

8282849: Loom: Fix minimal debug non-PCH build failure
As described in JBS-8282849, this build failure was introduced by commit
bd25c94 and commit 4d27102. Adding the missing headers would fix it.

src/hotspot/share/classfile/javaClasses.inline.hpp
@@ -33,6 +33,9 @@
 #include "oops/oop.inline.hpp"
 #include "oops/oopsHierarchy.hpp"
 #include "oops/typeArrayOop.inline.hpp"
+#ifdef ASSERT
+#include "logging/log.hpp"
+#endif
 
 void java_lang_String::set_coder(oop string, jbyte coder) {
   string->byte_field_put(_coder_offset, coder);

src/hotspot/share/oops/instanceStackChunkKlass.cpp
@@ -46,6 +46,9 @@
 #include "utilities/bitMap.hpp"
 #include "utilities/globalDefinitions.hpp"
 #include "utilities/macros.hpp"
+#ifdef ASSERT
+#include "logging/logStream.hpp"
+#endif
 
 #define FIX_DERIVED_POINTERS true
 