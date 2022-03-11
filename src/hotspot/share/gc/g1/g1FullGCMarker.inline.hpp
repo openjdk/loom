SaTaSo/jdk

Simplify GC code to deal more explicitly with stack chunks

src/hotspot/share/gc/g1/g1FullGCMarker.inline.hpp
@@ -35,6 +35,7 @@
 #include "gc/g1/g1FullGCOopClosures.inline.hpp"
 #include "gc/g1/g1RegionMarkStatsCache.hpp"
 #include "gc/g1/g1StringDedup.hpp"
+#include "gc/shared/continuationGCSupport.inline.hpp"
 #include "gc/shared/preservedMarks.inline.hpp"
 #include "gc/shared/stringdedup/stringDedup.hpp"
 #include "oops/access.inline.hpp"
@@ -66,6 +67,8 @@ inline bool G1FullGCMarker::mark_object(oop obj) {
       G1StringDedup::is_candidate_from_mark(obj)) {
     _string_dedup_requests.add(obj);
   }
+
+  ContinuationGCSupport::shrink_stack_chunk(obj);
 
   // Collect live words.
   _mark_stats_cache.add_live_words(obj);
@@ -131,6 +134,7 @@ void G1FullGCMarker::follow_array_chunk(objArrayOop array, int index) {
 
 inline void G1FullGCMarker::follow_object(oop obj) {
   assert(_bitmap->is_marked(obj), "should be marked");
+
   if (obj->is_objArray()) {
     // Handle object arrays explicitly to allow them to
     // be split into chunks if needed.

src/hotspot/share/gc/parallel/psPromotionManager.cpp
@@ -29,6 +29,7 @@
 #include "gc/parallel/psOldGen.hpp"
 #include "gc/parallel/psPromotionManager.inline.hpp"
 #include "gc/parallel/psScavenge.inline.hpp"
+#include "gc/shared/continuationGCSupport.inline.hpp"
 #include "gc/shared/gcTrace.hpp"
 #include "gc/shared/preservedMarks.inline.hpp"
 #include "gc/shared/taskqueue.inline.hpp"
@@ -343,6 +344,7 @@ oop PSPromotionManager::oop_promotion_failed(oop obj, markWord obj_mark) {
     // restoration. This way we don't have to walk the young-gen to locate
     // these promotion-failed objs.
     _preserved_marks->push_always(obj, obj_mark);
+    ContinuationGCSupport::shrink_stack_chunk(obj);
   }  else {
     // We lost, someone else "owns" this object
     guarantee(obj->is_forwarded(), "Object must be forwarded if the cas failed.");

src/hotspot/share/gc/serial/markSweep.inline.hpp
@@ -30,6 +30,7 @@
 #include "classfile/classLoaderData.inline.hpp"
 #include "classfile/javaClasses.inline.hpp"
 #include "code/nmethod.hpp"
+#include "gc/shared/continuationGCSupport.inline.hpp"
 #include "gc/serial/serialStringDedup.hpp"
 #include "memory/universe.hpp"
 #include "oops/markWord.hpp"
@@ -51,6 +52,8 @@ inline void MarkSweep::mark_object(oop obj) {
   // and overwrite the mark.  We'll restore it at the end of markSweep.
   markWord mark = obj->mark();
   obj->set_mark(markWord::prototype().set_marked());
+
+  ContinuationGCSupport::shrink_stack_chunk(obj);
 
   if (obj->mark_must_be_preserved(mark)) {
     preserve_mark(obj, mark);

openjdk/loom

src/hotspot/share/gc/g1/g1FullGCMarker.inline.hpp
@@ -35,6 +35,7 @@
 #include "gc/g1/g1FullGCOopClosures.inline.hpp"
 #include "gc/g1/g1RegionMarkStatsCache.hpp"
 #include "gc/g1/g1StringDedup.hpp"
+#include "gc/shared/continuationGCSupport.inline.hpp"
 #include "gc/shared/preservedMarks.inline.hpp"
 #include "gc/shared/stringdedup/stringDedup.hpp"
 #include "oops/access.inline.hpp"
@@ -66,6 +67,8 @@ inline bool G1FullGCMarker::mark_object(oop obj) {
       G1StringDedup::is_candidate_from_mark(obj)) {
     _string_dedup_requests.add(obj);
   }
+
+  ContinuationGCSupport::transform_stack_chunk(obj);
 
   // Collect live words.
   _mark_stats_cache.add_live_words(obj);

src/hotspot/share/gc/parallel/psPromotionManager.cpp
@@ -29,6 +29,7 @@
 #include "gc/parallel/psOldGen.hpp"
 #include "gc/parallel/psPromotionManager.inline.hpp"
 #include "gc/parallel/psScavenge.inline.hpp"
+#include "gc/shared/continuationGCSupport.inline.hpp"
 #include "gc/shared/gcTrace.hpp"
 #include "gc/shared/preservedMarks.inline.hpp"
 #include "gc/shared/taskqueue.inline.hpp"
@@ -343,6 +344,7 @@ oop PSPromotionManager::oop_promotion_failed(oop obj, markWord obj_mark) {
     // restoration. This way we don't have to walk the young-gen to locate
     // these promotion-failed objs.
     _preserved_marks->push_always(obj, obj_mark);
+    ContinuationGCSupport::transform_stack_chunk(obj);
   }  else {
     // We lost, someone else "owns" this object
     guarantee(obj->is_forwarded(), "Object must be forwarded if the cas failed.");

src/hotspot/share/gc/serial/markSweep.inline.hpp
@@ -30,6 +30,7 @@
 #include "classfile/classLoaderData.inline.hpp"
 #include "classfile/javaClasses.inline.hpp"
 #include "code/nmethod.hpp"
+#include "gc/shared/continuationGCSupport.inline.hpp"
 #include "gc/serial/serialStringDedup.hpp"
 #include "memory/universe.hpp"
 #include "oops/markWord.hpp"
@@ -51,6 +52,8 @@ inline void MarkSweep::mark_object(oop obj) {
   // and overwrite the mark.  We'll restore it at the end of markSweep.
   markWord mark = obj->mark();
   obj->set_mark(markWord::prototype().set_marked());
+
+  ContinuationGCSupport::transform_stack_chunk(obj);
 
   if (obj->mark_must_be_preserved(mark)) {
     preserve_mark(obj, mark);