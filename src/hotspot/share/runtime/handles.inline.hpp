35250917c5a9fca5b7a3159339a0418058fc6487
Ron Pressler <ron.pressler@oracle.com>
2022-04-01 13:27:04 +0100

    Add comment explaining why we don't have a barrier in Handle::replace

--- src/hotspot/share/runtime/handles.inline.hpp
+++ src/hotspot/share/runtime/handles.inline.hpp
@@ -44,6 +44,9 @@ inline Handle::Handle(Thread* thread, oop obj, bool allocNull) {
 }
 
 inline void Handle::replace(oop obj) {
+  // Unlike in OopHandle::replace, we shouldn't use a barrier here.
+  // OopHandle has its storage in OopStorage, which is walked concurrently and uses barriers.
+  // Handle is thread private, and iterated by Thread::oops_do, which is why it shouldn't have any barriers at all.
   assert(_handle != NULL, "should not use replace");
   *_handle = obj;
 }
