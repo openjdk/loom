b9db902a9e22f92a13c7078dddde7dd85e75a647
Alan Bateman <alan.bateman@oracle.com>
2022-10-23 10:39:36 +0100

    Improve ScopedValue tests

--- test/jdk/jdk/incubator/concurrent/ScopedValue/Basic.java
+++ test/jdk/jdk/incubator/concurrent/ScopedValue/Basic.java
@@ -29,30 +29,58 @@
  */
 
 import jdk.incubator.concurrent.ScopedValue;
-import jdk.incubator.concurrent.StructureViolationException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.*;
 
+@Test
 public class Basic {
 
-    @Test
-    public void testUnbound1() {
-        ScopedValue<String> v = ScopedValue.newInstance();
-        assertFalse(v.isBound());
-        assertThrows(NoSuchElementException.class, () -> v.get());
-    }
-
-    @Test
-    public void testOrElse() {
+    /**
+     * Test isBound from runnable op.
+     */
+    public void testIsBoundInRun() {
         ScopedValue<String> name = ScopedValue.newInstance();
         assertFalse(name.isBound());
+        ScopedValue.where(name, "fred", () -> assertTrue(name.isBound()));
+    }
+
+    /**
+     * Test isBound from callable op.
+     */
+    public void testIsBoundInCall() throws Exception {
+        ScopedValue<String> name = ScopedValue.newInstance();
+        assertFalse(name.isBound());
+        boolean isBound = ScopedValue.where(name, "fred", name::isBound);
+        assertTrue(isBound);
+    }
+
+    /**
+     * Test get from runnable op.
+     */
+    public void testGetInRun() {
+        ScopedValue<String> name = ScopedValue.newInstance();
+        assertThrows(NoSuchElementException.class, name::get);
+        ScopedValue.where(name, "fred", () -> assertEquals(name.get(), "fred"));
+    }
+
+    /**
+     * Test get from callable op.
+     */
+    public void testGetInCall() throws Exception {
+        ScopedValue<String> name = ScopedValue.newInstance();
+        assertThrows(NoSuchElementException.class, name::get);
+        String value = ScopedValue.where(name, "fred", name::get);
+        assertEquals(value, "fred");
+    }
+
+    /**
+     * Test orElse from runnable op.
+     */
+    public void testOrElseInRun() {
+        ScopedValue<String> name = ScopedValue.newInstance();
         assertTrue(name.orElse(null) == null);
         assertEquals(name.orElse("default"), "default");
         ScopedValue.where(name, "fred", () -> {
@@ -61,33 +89,86 @@ public class Basic {
         });
     }
 
-    @Test
-    public void testOrElseThrow() {
+    /**
+     * Test orElse from callable op.
+     */
+    public void testOrElseInCall() throws Exception {
         ScopedValue<String> name = ScopedValue.newInstance();
+        assertTrue(name.orElse(null) == null);
+        assertEquals(name.orElse("default"), "default");
+        var ignore = ScopedValue.where(name, "fred", () -> {
+            assertEquals(name.orElse(null), "fred");
+            assertEquals(name.orElse("default"), "fred");
+            return null;
+        });
+    }
+
+    /**
+     * Test orElseThrow from runnable op.
+     */
+    public void testOrElseThrowInRun() {
+        class FooException extends RuntimeException { }
+        ScopedValue<String> name = ScopedValue.newInstance();
+        assertThrows(FooException.class, () -> name.orElseThrow(FooException::new));
+        ScopedValue.where(name, "fred", () -> {
+            assertEquals(name.orElseThrow(FooException::new), "fred");
+        });
+    }
+
+    /**
+     * Test orElseThrow from callable op.
+     */
+    public void testOrElseThrowInCall() throws Exception {
+        class FooException extends RuntimeException { }
+        ScopedValue<String> name = ScopedValue.newInstance();
+        assertThrows(FooException.class, () -> name.orElseThrow(FooException::new));
+        var ignore = ScopedValue.where(name, "fred", () -> {
+            assertEquals(name.orElseThrow(FooException::new), "fred");
+            return null;
+        });
+    }
+
+    /**
+     * Test more than one binding in runnable op.
+     */
+    public void testMultipleBindingsInRun() {
+        ScopedValue<String> name = ScopedValue.newInstance();
+        ScopedValue<Integer> age = ScopedValue.newInstance();
+        ScopedValue.where(name, "fred").where(age, 100).run(() -> {
+            assertTrue(name.isBound());
+            assertTrue(age.isBound());
+            assertEquals(name.get(), "fred");
+            assertEquals((int) age.get(), 100);
+        });
         assertFalse(name.isBound());
-        assertThrows(IllegalStateException.class, () -> name.orElseThrow(IllegalStateException::new));
-        assertThrows(NullPointerException.class, () -> name.orElseThrow(null));
-        ScopedValue.where(name, "fred", () -> {
-            assertEquals(name.orElseThrow(IllegalStateException::new), "fred");
-            assertThrows(NullPointerException.class, () -> name.orElseThrow(null));
-        });
+        assertFalse(age.isBound());
     }
 
-    @Test
-    public void testRunWithBinding1() {
+    /**
+     * Test more than one binding in callable op.
+     */
+    public void testMultipleBindingsInCall() throws Exception {
+        ScopedValue<String> name = ScopedValue.newInstance();
+        ScopedValue<Integer> age = ScopedValue.newInstance();
+        var ignore = ScopedValue.where(name, "fred").where(age, 100).call(() -> {
+            assertTrue(name.isBound());
+            assertTrue(age.isBound());
+            assertEquals(name.get(), "fred");
+            assertEquals((int) age.get(), 100);
+            return null;
+        });
+        assertFalse(name.isBound());
+        assertFalse(age.isBound());
+    }
+
+    /**
+     * Test rebinding in a runnable task.
+     */
+    public void testRebindingInRun() {
         ScopedValue<String> name = ScopedValue.newInstance();
         ScopedValue.where(name, "fred", () -> {
             assertTrue(name.isBound());
-            assertTrue("fred".equals(name.get()));
-        });
-    }
-
-    @Test
-    public void testRunWithBinding2() {
-        ScopedValue<String> name = ScopedValue.newInstance();
-        ScopedValue.where(name, "fred", () -> {
-            assertTrue(name.isBound());
-            assertTrue("fred".equals(name.get()));
+            assertEquals(name.get(), "fred");
 
             ScopedValue.where(name, "joe", () -> {
                 assertTrue(name.isBound());
@@ -95,25 +176,39 @@ public class Basic {
             });
 
             assertTrue(name.isBound());
-            assertTrue("fred".equals(name.get()));
+            assertEquals(name.get(), "fred");
         });
+        assertFalse(name.isBound()); // no longer bound
     }
 
-    @Test
-    public void testRunWithBinding3() {
+    /**
+     * Test rebinding from null to another value in a runnable task.
+     */
+    public void testRebindingFromNullInRun() {
         ScopedValue<String> name = ScopedValue.newInstance();
         ScopedValue.where(name, null, () -> {
             assertTrue(name.isBound());
+            assertEquals(name.get(), null);
+
+            ScopedValue.where(name, "joe", () -> {
+                assertTrue(name.isBound());
+                assertTrue("joe".equals(name.get()));
+            });
+
+            assertTrue(name.isBound());
             assertTrue(name.get() == null);
         });
+        assertFalse(name.isBound()); // no longer bound
     }
 
-    @Test
-    public void testRunWithBinding4() {
+    /**
+     * Test rebinding to null in a runnable task.
+     */
+    public void testRebindingToNullInRun() {
         ScopedValue<String> name = ScopedValue.newInstance();
         ScopedValue.where(name, "fred", () -> {
             assertTrue(name.isBound());
-            assertTrue("fred".equals(name.get()));
+            assertEquals(name.get(), "fred");
 
             ScopedValue.where(name, null, () -> {
                 assertTrue(name.isBound());
@@ -121,85 +216,133 @@ public class Basic {
             });
 
             assertTrue(name.isBound());
-            assertTrue("fred".equals(name.get()));
+            assertEquals(name.get(), "fred");
         });
+        assertFalse(name.isBound()); // no longer bound
     }
 
-    @Test
-    public void testRunWithBinding9() {
+    /**
+     * Test rebinding in a callable task.
+     */
+    public void testRebindingInCall() throws Exception {
         ScopedValue<String> name = ScopedValue.newInstance();
-        assertThrows(NullPointerException.class,
-                     () -> ScopedValue.where(name, "fred", (Runnable) null));
-    }
-
-    @Test
-    public void testCallWithBinding1() throws Exception {
-        ScopedValue<String> name = ScopedValue.newInstance();
-        int result = ScopedValue.where(name, "fred", () -> {
+        var ignore1 = ScopedValue.where(name, "fred", () -> {
             assertTrue(name.isBound());
-            String value = name.get();
-            assertTrue("fred".equals(value));
-            return 1;
-        });
-        assertTrue(result == 1);
-    }
+            assertEquals(name.get(), "fred");
 
-    @Test
-    public void testCallWithBinding2() throws Exception {
-        ScopedValue<String> name = ScopedValue.newInstance();
-        int result1 = ScopedValue.where(name, "fred", () -> {
-            assertTrue(name.isBound());
-            String value1 = name.get();
-            assertTrue("fred".equals(value1));
-
-            int result2 = ScopedValue.where(name, "joe", () -> {
+            var ignore2 = ScopedValue.where(name, "joe", () -> {
                 assertTrue(name.isBound());
-                String value2 = name.get();
-                assertTrue("joe".equals(value2));
-                return 2;
+                assertTrue("joe".equals(name.get()));
+                return null;
             });
-            assertTrue(result2 == 2);
 
-            return 1;
+            assertTrue(name.isBound());
+            assertEquals(name.get(), "fred");
+            return null;
         });
-        assertTrue(result1 == 1);
+        assertFalse(name.isBound()); // no longer bound
     }
 
-    @Test
-    public void testCallWithBinding3() throws Exception {
+    /**
+     * Test rebinding from null to another value in a callable task.
+     */
+    public void testRebindingFromNullInCall() throws Exception {
         ScopedValue<String> name = ScopedValue.newInstance();
-        int result = ScopedValue.where(name, null, () -> {
+        var ignore1 = ScopedValue.where(name, null, () -> {
+            assertTrue(name.isBound());
+            assertEquals(name.get(), null);
+
+            var ignore2 = ScopedValue.where(name, "joe", () -> {
+                assertTrue(name.isBound());
+                assertTrue("joe".equals(name.get()));
+                return null;
+            });
+
             assertTrue(name.isBound());
             assertTrue(name.get() == null);
-            return 1;
+            return null;
         });
-        assertTrue(result == 1);
+        assertFalse(name.isBound()); // no longer bound
     }
 
-    @Test
-    public void testCallWithBinding4() throws Exception {
+    /**
+     * Test rebinding to null in a callable task.
+     */
+    public void testRebindingToNullInCall() throws Exception {
         ScopedValue<String> name = ScopedValue.newInstance();
-        int result1 = ScopedValue.where(name, "fred", () -> {
+        var ignore1 = ScopedValue.where(name, "fred", () -> {
             assertTrue(name.isBound());
-            String value1 = name.get();
-            assertTrue("fred".equals(value1));
+            assertEquals(name.get(), "fred");
 
-            int result2 = ScopedValue.where(name, null, () -> {
+            var ignore2 = ScopedValue.where(name, null, () -> {
                 assertTrue(name.isBound());
                 assertTrue(name.get() == null);
-                return 2;
+                return null;
             });
-            assertTrue(result2 == 2);
 
-            return 1;
+            assertTrue(name.isBound());
+            assertEquals(name.get(), "fred");
+            return null;
         });
-        assertTrue(result1 == 1);
+        assertFalse(name.isBound()); // no longer bound
     }
 
-    @Test
-    public void testCallWithBinding9() throws Exception {
+    /**
+     * Test runnable op throwing exception.
+     */
+    public void testRunThrows() {
+        class FooException extends RuntimeException { }
         ScopedValue<String> name = ScopedValue.newInstance();
-        assertThrows(NullPointerException.class,
-                     () -> ScopedValue.where(name, "fred", (Callable) null));
+        Runnable op = () -> { throw new FooException(); };
+        assertThrows(FooException.class, () -> ScopedValue.where(name, "fred", op));
+        assertFalse(name.isBound()); // no longer bound
+    }
+
+    /**
+     * Test callable op throwing exception.
+     */
+    public void testCallThrows() {
+        class FooException extends RuntimeException { }
+        ScopedValue<String> name = ScopedValue.newInstance();
+        Callable<Void> op = () -> { throw new FooException(); };
+        assertThrows(FooException.class, () -> ScopedValue.where(name, "fred", op));
+        assertFalse(name.isBound()); // no longer bound
+    }
+
+    /**
+     * Test Carrier.get.
+     */
+    public void testCarrierGet() throws Exception {
+        ScopedValue<String> name = ScopedValue.newInstance();
+        ScopedValue<Integer> age = ScopedValue.newInstance();
+
+        // one scoped value
+        var carrier1 = ScopedValue.where(name, "fred");
+        assertEquals(carrier1.get(name), "fred");
+        assertThrows(NoSuchElementException.class, () -> carrier1.get(age));
+
+        // two scoped values
+        var carrier2 = carrier1.where(age, 20);
+        assertEquals(carrier2.get(name), "fred");
+        assertEquals((int) carrier2.get(age), 20);
+    }
+
+    /**
+     * Test NullPointerException.
+     */
+    public void testNullPointerException() {
+        ScopedValue<String> name = ScopedValue.newInstance();
+
+        assertThrows(NullPointerException.class, () -> ScopedValue.where(null, "value"));
+        assertThrows(NullPointerException.class, () -> ScopedValue.where(null, "value", () -> { }));
+        assertThrows(NullPointerException.class, () -> ScopedValue.where(null, "value", () -> null));
+
+        assertThrows(NullPointerException.class, () -> name.orElseThrow(null));
+
+        var carrier = ScopedValue.where(name, "fred");
+        assertThrows(NullPointerException.class, () -> carrier.where(null, "value"));
+        assertThrows(NullPointerException.class, () -> carrier.get(null));
+        assertThrows(NullPointerException.class, () -> carrier.run(null));
+        assertThrows(NullPointerException.class, () -> carrier.call(null));
     }
 }
