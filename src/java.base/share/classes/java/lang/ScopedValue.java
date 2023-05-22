1c84a07124a4120206e87b88c2d43858d3bb3a4f
Alan Bateman <alan.bateman@oracle.com>
2023-05-22 19:43:26 +0100

    Undo

--- src/java.base/share/classes/java/lang/ScopedValue.java
+++ src/java.base/share/classes/java/lang/ScopedValue.java
@@ -404,7 +404,7 @@ public final class ScopedValue<T> {
         // Carrier#call() in this thread because it needs neither
         // runtime bytecode generation nor any release fencing.
         private static final class CallableAdapter<V> implements Callable<V> {
-            private final Supplier<? extends V> s;
+            private Supplier<? extends V> s;
             CallableAdapter(Supplier<? extends V> s) {
                 this.s = s;
             }
