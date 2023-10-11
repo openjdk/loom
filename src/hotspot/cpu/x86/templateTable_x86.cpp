9bca6603204e94ffac4be450b73610cbaf9eed0f
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2023-10-11 18:49:13 +0000

    implement work around for interpreter profile data mismatch bug

--- src/hotspot/cpu/x86/templateTable_x86.cpp
+++ src/hotspot/cpu/x86/templateTable_x86.cpp
@@ -4438,8 +4438,10 @@ void TemplateTable::monitorenter() {
     ExternalAddress fetch_addr((address) Universe::object_monitorEnter_addr());
     __ movptr(method, fetch_addr);
 
-    __ profile_call(rax);
-    __ profile_arguments_type(rax, rbx, rbcp, false);
+    if (!ObjectMonitorMode::java()) {
+      __ profile_call(rax);
+      __ profile_arguments_type(rax, rbx, rbcp, false);
+    }
 
     __ jump_from_interpreted(method, rdx);
 
@@ -4613,8 +4615,10 @@ void TemplateTable::monitorexit() {
     ExternalAddress fetch_addr((address) Universe::object_monitorExit_addr());
     __ movptr(method, fetch_addr);
 
-    __ profile_call(rax);
-    __ profile_arguments_type(rax, rbx, rbcp, false);
+    if (!ObjectMonitorMode::java()) {
+      __ profile_call(rax);
+      __ profile_arguments_type(rax, rbx, rbcp, false);
+    }
 
     __ jump_from_interpreted(rbx, rdx);
 
