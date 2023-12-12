493ca3315d868a88afb93a1b49b24aaac3acc7a8
Alan Bateman <alan.bateman@oracle.com>
2023-12-12 14:39:50 +0000

    Remove testPinnedCausedBySynchronizedBlock

--- test/jdk/java/lang/Thread/virtual/TracePinnedThreads.java
+++ test/jdk/java/lang/Thread/virtual/TracePinnedThreads.java
@@ -59,24 +59,6 @@ class TracePinnedThreads {
     private static native void invokePark();
 
     /**
-     * Test parking inside synchronized block.
-     */
-    @Test
-    void testPinnedCausedBySynchronizedBlock() throws Exception {
-        String output = run(() -> {
-            synchronized (lock) {
-                park();
-            }
-        });
-        if (Platform.isX64()) {
-            assertDoesNotContain(output, "<== monitors:1");
-        } else {
-            assertContains(output, "<== monitors:1");
-        }
-        assertDoesNotContain(output, "(Native Method)");
-    }
-
-    /**
      * Test parking with native frame on stack.
      */
     @Test
