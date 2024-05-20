fef916cc4271bbf9786fd89319999d885cc6f945
Alan Bateman <alan.bateman@oracle.com>
2024-05-20 10:30:57 +0100

    Clarify comment

--- test/jdk/java/lang/Thread/virtual/JfrEvents.java
+++ test/jdk/java/lang/Thread/virtual/JfrEvents.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -164,7 +164,7 @@ class JfrEvents {
     }
 
     /**
-     * Test jdk.VirtualThreadPinned event when waiting with Object.wait.
+     * Test jdk.VirtualThreadPinned event when waiting with Object.wait while pinned.
      */
     @ParameterizedTest
     @ValueSource(booleans = { true, false })
